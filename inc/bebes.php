8de55b85d4cd349b39c8b50f6ef4aebff61a1263
David Parro Rubio <davidparro@astibot.es>
2017-03-10 12:57:16 +0100

    Cambio a index.html

--- inc/bebes.php
+++ inc/bebes.php
@@ -16,17 +16,17 @@
   <body>
     <header>
       <div class="menu2">
-        <ul>
-          <li><a href="../index.php">INICIO</a></li>
-          <li><a href="../index.php #secciones">SECCIONES</a></li>
-          <li><a href="../index.php #blog">BLOG</a></li>
-          <li><img class='logo' src="../img/logo2.png" alt="logo"></li>
-          <li><a href="../index.php #sobremi">SOBRE MI</a></li>
-          <li><a href="../index.php #gmap">UBICACIÓN</a></li>
-          <li><a href="../index.php #contacto">CONTACTO</a></li>
-        </ul>
-        <a href="#acceso">ACCESO CLIENTES</a>
-      </div>
+  			<ul>
+  				<li><a href="../index.html">INICIO</a></li>
+  				<li><a href="../index.html #secciones">SECCIONES</a></li>
+  				<li><a href="../index.html #blog">BLOG</a></li>
+  				<li><img class='logo' src="../img/logo2.png" alt="logo"></li>
+  				<li><a href="../index.html #sobremi">SOBRE MI</a></li>
+  				<li><a href="../index.html #gmap">UBICACIÓN</a></li>
+  				<li><a href="../index.html #contacto">CONTACTO</a></li>
+  			</ul>
+  			<a href="#acceso">ACCESO CLIENTES</a>
+  		</div>
       <div class="flexslider">
         <ul class="slides">
           <li data-thumb="../img/bautizos/Album1/16712744833_4d21865ed8_o.jpg">
