5a90b1cd34bbde520574a3020038da343e78ba4d
David Parro Rubio <davidparro@astibot.es>
2017-03-10 12:55:53 +0100

    Cambios varios

---
+++ inc/bebes.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="icon" href="../favicon.png" type="image/png" />
+    <link rel="stylesheet" href="../css/normalize.css" type="text/css">
+    <link rel="stylesheet" href="../css/main.css" type="text/css">
+    <link rel="stylesheet" href="../flexslider/flexslider.css" type="text/css">
+    <script src="../js/jquery-3.1.1.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+    <script src="../flexslider/jquery.flexslider-min.js"></script>
+    <script src="../js/flexslider.js"></script>
+    <script src="https://use.fontawesome.com/01dd6c6b33.js"></script>
+    <title>Bautizos & Bebés</title>
+  </head>
+  <body>
+    <header>
+      <div class="menu2">
+        <ul>
+          <li><a href="../index.php">INICIO</a></li>
+          <li><a href="../index.php #secciones">SECCIONES</a></li>
+          <li><a href="../index.php #blog">BLOG</a></li>
+          <li><img class='logo' src="../img/logo2.png" alt="logo"></li>
+          <li><a href="../index.php #sobremi">SOBRE MI</a></li>
+          <li><a href="../index.php #gmap">UBICACIÓN</a></li>
+          <li><a href="../index.php #contacto">CONTACTO</a></li>
+        </ul>
+        <a href="#acceso">ACCESO CLIENTES</a>
+      </div>
+      <div class="flexslider">
+        <ul class="slides">
+          <li data-thumb="../img/bautizos/Album1/16712744833_4d21865ed8_o.jpg">
+            <img src="../img/bautizos/Album1/16712744833_4d21865ed8_o.jpg" />
+          </li>
+          <li data-thumb="../img/bautizos/Album1/17081588350_41cb0de454_o.jpg">
+            <img src="../img/bautizos/Album1/17081588350_41cb0de454_o.jpg" />
+          </li>
+          <li data-thumb="../img/bautizos/Album1/17243178166_67d032b1a6_o.jpg">
+            <img src="../img/bautizos/Album1/17243178166_67d032b1a6_o.jpg" />
+          </li>
+          <li data-thumb="../img/bautizos/Album1/17267373382_a91acc9b8c_o.jpg">
+            <img src="../img/bautizos/Album1/17267373382_a91acc9b8c_o.jpg" />
+          </li>
+<!--
+          <li data-thumb="../img/bautizos/Album1/17307028356_b18975571c_o.jpg">
+            <img src="../img/bautizos/Album1/17307028356_b18975571c_o.jpg" />
+          </li>
+-->
+        </ul>
+      </div>
+    </header>
+    <div id="cuerpo">
+      <h1>BEBES</h1>
+      <p>En NDC fotografía hacemos fotografía de premamá, recién nacido, bautizo y infantil,
+        Creando así recuerdos imborrables de cada una de las etapas por las que pasan vuestros pequeños.
+        Siempre hacemos que la sesión sea divertida por encima de todas las cosas.
+        Estas sesiones se realizan en una mañana o en una tarde, y suelen durar entre dos y tres horas,
+        sobre todo trabajando con niños no debemos tener prisas si queremos buenos resultados.
+        Normalmente las realizo en Aldeamayor de Sam Martin dentro de un entorno privilegiado,
+        aunque también es posible en vuestras propia casa (en sesión de recién nacido siempre es en
+        la casa del bebe para no casarle de su entorno, y es preferible realizarlas en los primeros 15 días).
+        Consulte nuestras tarifas de los reportajes de premamá, recién nacido, bautizo y infantil.
+      </p>
+    </div>
+    <footer>
+      <p><img class='cr' src='../img/cr.png'>Nathalia Dias Campos - Todos los derechos reservados - <a href='../inc/cookies.html'>Política de cookies</a> - <a href='../inc/avisoLegal.html'>Aviso Legal</a></p>
+    </footer>
+    <a href='#' class='subir'><img src="../img/flecha.png" alt="flecha"></a>
+  </body>
+</html>
