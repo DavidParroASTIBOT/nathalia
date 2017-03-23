d286408b04ed75bb2b251e4f17aff0d82adf7dd8
David <davidparro@astibot.es>
2017-03-23 15:32:39 +0100

    img bug fix

--- js/slider.js
+++ js/slider.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     c=1;
     $('#slider').append("<img src='./img/slider/slider"+c+".jpg' alt='fondo' />");
     function fondo(){
-      n=6;
+      n=7;
       if(c==n){
         c=1;
       }else{
